ceefb101f813305c9a4b7bc2722e2d3ea915f9eb
SvetlanaGrishel <svetlanagrishel@Noutbuk-SvetlanaGrishel.local>
2026-01-05 14:37:02 +0300

    feat: Add logging to Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("==> Entering method: main with arguments: {}", (Object) args);
+        //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+        // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        logger.info("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            logger.info("i = " + i);
+        }
+        logger.info("<== Exiting method: main with result: {}", "void");
     }
 }
